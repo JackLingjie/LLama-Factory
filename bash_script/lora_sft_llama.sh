1264dd530db837f2d2bafdaf1ee6e4d7c55c55f9
Jack-lingjie <lingjiejiang@stu.pku.edu.cn>
2024-08-06 19:40:13 -0700

    add scirpt of training tulu by dpo

--- bash_script/lora_sft_llama.sh
+++ bash_script/lora_sft_llama.sh
@@ -10,3 +10,6 @@ llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
 llamafactory-cli train examples/train_lora/tulu_llama3_lora_sft.yaml
 
 FORCE_TORCHRUN=1 llamafactory-cli train examples/train_lora/tulu_lora_sft_ds2.yaml
+
+# dpo train
+llamafactory-cli train examples/train_lora/tulu_lora_dpo.yaml
