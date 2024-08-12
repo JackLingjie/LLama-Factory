204ff79997bec39a7316c6a7271ab15b572820f3
Jack-lingjie <lingjiejiang@stu.pku.edu.cn>
2024-08-12 00:42:57 -0700

    merge lora checkpoints

--- bash_script/lora_sft_llama.sh
+++ bash_script/lora_sft_llama.sh
@@ -15,12 +15,16 @@ FORCE_TORCHRUN=1 llamafactory-cli train examples/train_lora/tulu_lora_sft_ds2.ya
 llamafactory-cli train examples/train_lora/tulu_lora_dpo.yaml
 
 torchrun --nproc_per_node=4 src/train.py bash_script/tulu_lora_dpo.yaml
+torchrun --nproc_per_node=4 src/train.py bash_script/tulu_lora_dpo_test.yaml
+
 torchrun --nproc_per_node=4 src/train.py bash_script/tulu_lora_dpo_job.yaml
 
-#job dpo
+#job dpo train 
 torchrun --nproc_per_node=8 src/train.py bash_script/tulu_lora_dpo_job.yaml
-
 llamafactory-cli train bash_script/tulu_lora_dpo_job.yaml
 
 
 accelerate launch --config_file bash_script/config/fsdp_config.yaml src/train.py bash_script/tulu_lora_dpo.yaml
+
+# export model
+llamafactory-cli export bash_script/merge_lora_sft.yaml
