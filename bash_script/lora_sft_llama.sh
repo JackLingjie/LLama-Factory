fc2d2179cd4591a4a5909d59c8190c3582b84787
Jack-lingjie <lingjiejiang@stu.pku.edu.cn>
2024-08-13 23:46:48 -0700

    add training script with default template of llama3.1-base

--- bash_script/lora_sft_llama.sh
+++ bash_script/lora_sft_llama.sh
@@ -9,6 +9,8 @@ llamafactory-cli train examples/train_lora/llama3_lora_sft.yaml
 
 llamafactory-cli train examples/train_lora/tulu_llama3_lora_sft.yaml
 
+llamafactory-cli train bash_script/tulu_lora_sft_ds2_local_test.yaml
+
 FORCE_TORCHRUN=1 llamafactory-cli train examples/train_lora/tulu_lora_sft_ds2.yaml
 
 # local dpo train
@@ -26,5 +28,8 @@ llamafactory-cli train bash_script/tulu_lora_dpo_job.yaml
 
 accelerate launch --config_file bash_script/config/fsdp_config.yaml src/train.py bash_script/tulu_lora_dpo.yaml
 
-# export model
+# export sft model
 llamafactory-cli export bash_script/merge_lora_sft.yaml
+
+# export dpo model
+llamafactory-cli export bash_script/merge_dpo_tulu.yaml
