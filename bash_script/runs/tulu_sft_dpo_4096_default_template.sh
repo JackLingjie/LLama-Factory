7ce6075f984bedef3eec5d3bf7c6be87c2f6db4f
Jack-lingjie <lingjiejiang@stu.pku.edu.cn>
2024-08-16 08:18:13 -0700

    modify sft and dpo job script

--- bash_script/runs/tulu_sft_dpo_4096_default_template.sh
+++ bash_script/runs/tulu_sft_dpo_4096_default_template.sh
@@ -7,9 +7,11 @@ DPO_CONFIG="bash_script/tulu_lora_dpo_4096_default_template_job.yaml"
 
 echo "Runing sft stage"
 torchrun --nproc_per_node=$NUM_PROC src/train.py $SFT_CONFIG
+# torchrun --nproc_per_node=8 src/train.py "bash_script/tulu_lora_sft_4096_default_template_job.yaml"
 
 echo "sleep 60"
 sleep 60
 
 echo "Runing dpo stage"
 torchrun --nproc_per_node=$NUM_PROC src/train.py $DPO_CONFIG
+torchrun --nproc_per_node=8 src/train.py "bash_script/tulu_lora_dpo_4096_default_template_job.yaml"
