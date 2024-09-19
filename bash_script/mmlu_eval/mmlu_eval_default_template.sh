2e0d57477faf8ec029b5e1c8ddb8d8f3da42f4d8
Jack-Lingjie <lingjiejiang@stu.pku.edu.cn>
2024-09-19 12:51:09 +0000

    change gpu info

--- bash_script/mmlu_eval/mmlu_eval_default_template.sh
+++ bash_script/mmlu_eval/mmlu_eval_default_template.sh
@@ -4,7 +4,7 @@ set -x
 DEFAULT_MODEL_NAME="Meta-Llama-3.1-8B-Instruct"
 MODEL_NAME=${1:-$DEFAULT_MODEL_NAME}
 
-llamafactory-cli eval \
+CUDA_VISIBLE_DEVICES=2,3 llamafactory-cli eval \
 --model_name_or_path /mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints/${MODEL_NAME} \
 --template default \
 --task mmlu_test \
