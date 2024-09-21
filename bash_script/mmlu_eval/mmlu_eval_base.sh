fd0a00fcf3bbae2c45421fbd2b6f215c0cf60580
Jack-Lingjie <lingjiejiang@stu.pku.edu.cn>
2024-09-21 14:19:01 +0000

    add base model

---
+++ bash_script/mmlu_eval/mmlu_eval_base.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+
+DEFAULT_MODEL_NAME="Meta-Llama-3.1-8B"
+MODEL_NAME=${1:-$DEFAULT_MODEL_NAME}
+
+CUDA_VISIBLE_DEVICES=0 llamafactory-cli eval \
+--model_name_or_path /mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints/${MODEL_NAME} \
+--template fewshot \
+--task mmlu \
+--split validation \
+--lang en \
+--n_shot 5 \
+--batch_size 16
