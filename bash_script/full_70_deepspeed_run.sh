6289bf27bf036be776b05f22b1fb0a8a8bdcfb28
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-10-12 11:20:23 +0000

    amd debug

--- bash_script/full_70_deepspeed_run.sh
+++ bash_script/full_70_deepspeed_run.sh
@@ -1,6 +1,6 @@
 deepspeed --num_gpus 8 --num_nodes 8 --hostfile bash_script/config_files/hostfile \
 src/train.py \
---model_name_or_path /mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints/Meta-Llama-3.1-70B-Instruct \
+--model_name_or_path /mnt/lingjiejiang/textual_aesthetics/model_checkpoint/sft_merge_checkpoints/Llama-3.1-70B \
 --stage sft \
 --do_train \
 --finetuning_type full \
@@ -11,13 +11,13 @@ src/train.py \
 --cutoff_len 2048 \
 --overwrite_cache \
 --preprocessing_num_workers 16 \
---output_dir /mnt/lingjiejiang/textual_aesthetics/exp/saves/glanchat_v2_70b_2048_default_template/fullft_lr5e6_e3_fx/sft \
+--output_dir /mnt/lingjiejiang/textual_aesthetics/exp/saves/glanchat_v2_llama_base_70b_2048_default_template_test/fullft_lr5e6_e3_fx/sft \
 --logging_steps 10 \
 --save_steps 500 \
 --plot_loss \
 --overwrite_output_dir \
 --per_device_train_batch_size 1 \
---gradient_accumulation_steps 8 \
+--gradient_accumulation_steps 1 \
 --learning_rate 5.0e-6 \
 --num_train_epochs 3.0 \
 --lr_scheduler_type cosine \
@@ -27,5 +27,5 @@ src/train.py \
 --val_size 0.1 \
 --per_device_eval_batch_size 1 \
 --eval_strategy steps \
---eval_steps 1000  \
+--eval_steps 500  \
 --deepspeed examples/deepspeed/ds_z3_config.json
