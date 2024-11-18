0b82dbc78dbf27a2c495e92d9797aef01c721797
buaahsh <buaahsh@foxmail.com>
2024-11-18 13:26:51 +0800

    sh

--- exp/ads/inf-g1.sh
+++ exp/ads/inf-g1.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-3000/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
+# azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-3000/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
 
 
 step=160000
@@ -10,10 +10,10 @@ for idx in 0; do
     start_idx=$((idx * step))
     end_idx=$((start_idx + step))
     echo "start_idx: $start_idx, end_idx: $end_idx"
-    python infer_batch.py \
+    python inference.py \
         --ckpt_folder "$HOME/checkpoints/$basefolder/checkpoint-3000" \
         --data_file "/data/users/shaohanh/demo/Validation_test_trunc256_gemma2_prompt.jsonl" \
         --dst_file "${basefolder}_imps_fbs_e2_preds_part_${idx}.txt" \
-        --start_idx $start_idx --end_idx $end_idx &
+        --batch_size 128 --start_idx $start_idx --end_idx $end_idx &
         # --data_file "$HOME/Datasets/L3Rel/Validation_test_trunc256_gemma2_prompt_shuf10k.jsonl" \
 done
