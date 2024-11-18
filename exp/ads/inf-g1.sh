9b5cf43e2570a94a6d3691acec0e012c4a755aac
buaahsh <buaahsh@foxmail.com>
2024-11-18 17:26:55 +0800

    sj

--- exp/ads/inf-g1.sh
+++ exp/ads/inf-g1.sh
@@ -2,14 +2,14 @@
 
 # azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-3000/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
 
-azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-1500/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
-azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-2000/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
-azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-2500/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
+# azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-1500/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
+# azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-2000/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
+# azcopy cp "https://msranlpintern.blob.core.windows.net/lingjiejiang/textual_aesthetics/exp/saves/bitnet_ads/bitnet_dv3_train_2.5m_trunc256_ql_default_template_2e5_e5_bsz2k/checkpoint-2500/?sv=2023-01-03&st=2024-11-15T09%3A47%3A56Z&se=2024-11-22T09%3A47%3A00Z&skoid=4ca952ec-968b-41ca-95cb-e9b2a5fe0a02&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2024-11-15T09%3A47%3A56Z&ske=2024-11-22T09%3A47%3A00Z&sks=b&skv=2023-01-03&sr=c&sp=racwdxltf&sig=N0JkiKyzWIp9KAgFlZj7orUFw9aebpYrH5kbf8OvceU%3D" "ads-e5" --recursive
 
 step=80000
-for idx in 0 1; do
+for idx in 0; do
     basefolder="ads-e5"
-    export CUDA_VISIBLE_DEVICES=$((idx + 2))
+    export CUDA_VISIBLE_DEVICES=$((idx + 3))
     start_idx=$((idx * step))
     end_idx=$((start_idx + step))
     echo "start_idx: $start_idx, end_idx: $end_idx"
@@ -21,9 +21,9 @@ for idx in 0 1; do
 done
 wait
 step=80000
-for idx in 0 1; do
+for idx in 0; do
     basefolder="ads-e5"
-    export CUDA_VISIBLE_DEVICES=$((idx + 2))
+    export CUDA_VISIBLE_DEVICES=$((idx + 3))
     start_idx=$((idx * step))
     end_idx=$((start_idx + step))
     echo "start_idx: $start_idx, end_idx: $end_idx"
@@ -35,9 +35,9 @@ for idx in 0 1; do
 done
 wait
 step=80000
-for idx in 0 1; do
+for idx in 0; do
     basefolder="ads-e5"
-    export CUDA_VISIBLE_DEVICES=$((idx + 2))
+    export CUDA_VISIBLE_DEVICES=$((idx + 3))
     start_idx=$((idx * step))
     end_idx=$((start_idx + step))
     echo "start_idx: $start_idx, end_idx: $end_idx"
@@ -52,10 +52,10 @@ echo "Done"
 
 echo "Calculating metrics"
 echo "python calc.py ads-e5_4000_imps_fbs_e2_preds_part_0.txt,ads-e5_4000_imps_fbs_e2_preds_part_1.txt"
-python calc.py ads-e5_4000_imps_fbs_e2_preds_part_1.txt,ads-e5_4000_imps_fbs_e2_preds_part_0.txt
+python calc.py ads-e5_1500_imps_fbs_e2_preds_part_1.txt,ads-e5_1500_imps_fbs_e2_preds_part_0.txt
 echo "python calc.py ads-e5_5000_imps_fbs_e2_preds_part_0.txt,ads-e5_5000_imps_fbs_e2_preds_part_1.txt"
-python calc.py ads-e5_5000_imps_fbs_e2_preds_part_1.txt,ads-e5_5000_imps_fbs_e2_preds_part_0.txt
+python calc.py ads-e5_2000_imps_fbs_e2_preds_part_1.txt,ads-e5_2000_imps_fbs_e2_preds_part_0.txt
 echo "python calc.py ads-e5_6000_imps_fbs_e2_preds_part_0.txt,ads-e5_6000_imps_fbs_e2_preds_part_1.txt"
-python calc.py ads-e5_6000_imps_fbs_e2_preds_part_1.txt,ads-e5_6000_imps_fbs_e2_preds_part_0.txt
+python calc.py ads-e5_2500_imps_fbs_e2_preds_part_1.txt,ads-e5_2500_imps_fbs_e2_preds_part_0.txt
 echo "python calc.py ads-e5_6300_imps_fbs_e2_preds_part_0.txt,ads-e5_6300_imps_fbs_e2_preds_part_1.txt"
 python calc.py ads-e5_6300_imps_fbs_e2_preds_part_1.txt,ads-e5_6300_imps_fbs_e2_preds_part_0.txt
